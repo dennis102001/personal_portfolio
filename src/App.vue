44fa302368391f3a7aff860b140a69d5bcd3053f
Dennis Torres <dennisbed1001@gmail.com>
2026-06-18 23:36:30 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="bg-blue-900">
+    <header class="border-b bg-white/90">
+      Menu here
+    </header>
+
+    <main>
+      <RouterView/>
+    </main>
+  </div>
+  
+
+
+  
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+
+  This is my portfolio
+  
+</template>
